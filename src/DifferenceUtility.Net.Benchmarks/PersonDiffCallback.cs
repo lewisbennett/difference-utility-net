c2e7b648b9166373b1a510c0efc6463bdeacc49f
Lewis <lewisbennett669@gmail.com>
2021-10-26 16:45:56 +0100

    Added integer ID benchmarks

--- src/DifferenceUtility.Net.Benchmarks/PersonDiffCallback.cs
+++ src/DifferenceUtility.Net.Benchmarks/PersonDiffCallback.cs
@@ -46,4 +46,46 @@ namespace DifferenceUtility.Net.Benchmarks
         }
         #endregion
     }
+    
+    public class PersonDiffCallback_Gen_Int : BaseDiffCallback<Person<int>>
+    {
+        #region Public Methods
+        /// <inheritdoc />
+        public override bool AreContentsTheSame(Person<int> oldItem, Person<int> newItem)
+        {
+            // Contents are the same if the property values match.
+            // This method is only invoked if AreItemsTheSame returns true, so we don't need to compare IDs.
+            return EqualityComparer<string>.Default.Equals(oldItem.FirstName, newItem.FirstName)
+                   && EqualityComparer<string>.Default.Equals(oldItem.LastName, newItem.LastName);
+        }
+
+        /// <inheritdoc />
+        public override bool AreItemsTheSame(Person<int> oldItem, Person<int> newItem)
+        {
+            // Items are the same if their persistant IDs are the same.
+            return oldItem.ID == newItem.ID;
+        }
+        #endregion
+    }
+
+    public class PersonDiffCallback_Gen2_Int : BaseDiffCallback<Person<int>, Person<int>>
+    {
+        #region Public Methods
+        /// <inheritdoc />
+        public override bool AreContentsTheSame(Person<int> oldItem, Person<int> newItem)
+        {
+            // Contents are the same if the property values match.
+            // This method is only invoked if AreItemsTheSame returns true, so we don't need to compare IDs.
+            return EqualityComparer<string>.Default.Equals(oldItem.FirstName, newItem.FirstName)
+                   && EqualityComparer<string>.Default.Equals(oldItem.LastName, newItem.LastName);
+        }
+
+        /// <inheritdoc />
+        public override bool AreItemsTheSame(Person<int> oldItem, Person<int> newItem)
+        {
+            // Items are the same if their persistant IDs are the same.
+            return oldItem.ID == newItem.ID;
+        }
+        #endregion
+    }
 }
