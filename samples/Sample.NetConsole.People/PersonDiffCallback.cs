f52866dcf64773a104c2f82d573c70e7017820e6
Lewis <lewisbennett669@gmail.com>
2021-12-07 16:17:20 +0000

    Improved people sample project

--- samples/Sample.NetConsole.People/PersonDiffCallback.cs
+++ samples/Sample.NetConsole.People/PersonDiffCallback.cs
@@ -2,30 +2,29 @@
 using Sample.Assets;
 using System.Collections.Generic;
 
-namespace Sample.NetConsole.People
+namespace Sample.NetConsole.People;
+
+public class PersonDiffCallback : BaseDiffCallback<Person, Person>
 {
-    public class PersonDiffCallback : BaseDiffCallback<Person, Person>
+    public override bool AreContentsTheSame(Person oldItem, Person newItem)
     {
-        public override bool AreContentsTheSame(Person oldItem, Person newItem)
-        {
-            // Contents are the same if the property values match.
-            // This method is only invoked if AreItemsTheSame returns true, so we don't need to compare IDs.
-            return EqualityComparer<string>.Default.Equals(oldItem.FirstName, newItem.FirstName)
-                && EqualityComparer<string>.Default.Equals(oldItem.LastName, newItem.LastName);
-        }
+        // Contents are the same if the property values match.
+        // This method is only invoked if AreItemsTheSame returns true, so we don't need to compare IDs.
+        return EqualityComparer<string>.Default.Equals(oldItem.FirstName, newItem.FirstName)
+            && EqualityComparer<string>.Default.Equals(oldItem.LastName, newItem.LastName);
+    }
 
-        public override bool AreItemsTheSame(Person oldItem, Person newItem)
-        {
-            // Items are the same if their persistant IDs are the same.
-            return oldItem.ID == newItem.ID;
-        }
+    public override bool AreItemsTheSame(Person oldItem, Person newItem)
+    {
+        // Items are the same if their persistant IDs are the same.
+        return oldItem.ID == newItem.ID;
+    }
         
-        public override void UpdateContents(Person item, Person dataSource)
-        {
-            base.UpdateContents(item, dataSource);
+    public override void UpdateContents(Person item, Person dataSource)
+    {
+        base.UpdateContents(item, dataSource);
 
-            item.FirstName = dataSource.FirstName;
-            item.LastName = dataSource.LastName;
-        }
+        item.FirstName = dataSource.FirstName;
+        item.LastName = dataSource.LastName;
     }
 }
--- samples/Sample.NetConsole.People/Program.cs
+++ samples/Sample.NetConsole.People/Program.cs
@@ -1,112 +1,68 @@
-﻿using DifferenceUtility.Net;
-using Sample.Assets;
-using System;
+﻿using System;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
-using System.Linq;
 using System.Threading.Tasks;
+using DifferenceUtility.Net;
+using Sample.Assets;
+using Sample.NetConsole.People;
 
-namespace Sample.NetConsole.People
+var data = new ObservableCollection<Person>();
+
+data.CollectionChanged += (s, e) =>
 {
-    public static class Program
+    switch (e.Action)
     {
-        public class CharWrapper
-        {
-            public char C { get; set; }
-        }
-        
-        // Main application entry point.
-        public static async Task Main()
-        {
-            // badfhlocz
-            // azhbdfc
-            
-            // nwpalgfbds
-            // mlfsgbadnhzx
-            
-            // var source = new ObservableCollection<CharWrapper>("nwpalgfbds".Select(x => new CharWrapper
-            // {
-            //     C = x
-            // }));
-            //
-            // var destination = "mlfsgbadnhzx".Select(x => new CharWrapper
-            // {
-            //     C = x
-            // });
-            //
-            // source.CollectionChanged += DataOnCollectionChanged;
-            //
-            // var c = new TestDiffCallback();
-            //
-            // var r = DiffUtil.CalculateDiff(source, destination, c);
-            //
-            // r.DispatchUpdatesTo(source);
-            //
-            // foreach (var x in source)
-            // {
-            //     Console.Write(x.C);
-            // }
-            //
-            // return;
-            
-            var data = new ObservableCollection<Person>();
-            var callback = new PersonDiffCallback();
-            
-            data.CollectionChanged += DataOnCollectionChanged;
-            
-            // Request collection of people.
-            var people = await API.GetPeopleAsync();
-            
-            // Calculate the difference between the new data, and the existing data.
-            var result = DiffUtil.CalculateDiff(data, people, callback);
-            
-            // Apply the changes to the data.
-            result.DispatchUpdatesTo(data);
-            
-            Console.WriteLine();
-            
-            foreach (var item in data)
-                Console.WriteLine(item.ToString());
-            
-            Console.WriteLine();
-            
-            // Some time has passed - the remote data has changed.
-            await Task.Delay(TimeSpan.FromSeconds(2));
-            
-            // Get the new data.
-            people = await API.GetPeopleAsync();
-            
-            result = DiffUtil.CalculateDiff(data, people, callback);
-            
-            result.DispatchUpdatesTo(data);
-            
-            Console.WriteLine();
-            
-            foreach (var item in data)
-                Console.WriteLine(item.ToString());
-            
-            Console.ReadLine();
-        }
+        case NotifyCollectionChangedAction.Add:
+            Console.WriteLine($"Item added at index: {e.NewStartingIndex}");
+            break;
 
-        private static void DataOnCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
-        {
-            switch (e.Action)
-            {
-                case NotifyCollectionChangedAction.Add:
-                    Console.WriteLine($"Item added at index: {e.NewStartingIndex}");
-                    break;
-
-                case NotifyCollectionChangedAction.Remove:
-                    Console.WriteLine($"Item removed at index: {e.OldStartingIndex}");
-                    break;
-                
-                case NotifyCollectionChangedAction.Move:
-                    Console.WriteLine($"Item moved from index: {e.OldStartingIndex} to {e.NewStartingIndex}");
-                    break;
-                
-                default:
-                    throw new NotImplementedException();
-            }
-        }
+        case NotifyCollectionChangedAction.Remove:
+            Console.WriteLine($"Item removed at index: {e.OldStartingIndex}");
+            break;
+    
+        case NotifyCollectionChangedAction.Move:
+            Console.WriteLine($"Item moved from index: {e.OldStartingIndex} to {e.NewStartingIndex}");
+            break;
+    
+        default:
+            throw new NotImplementedException();
     }
-}
+};
+
+// Request collection of people.
+var people = await API.GetPeopleAsync();
+
+var diffCallback = new PersonDiffCallback();
+
+// Calculate the difference between the old datasource and the new datasource.
+// We already know that the items source is empty, but DiffUtil can add items when there is no pre-existing data.
+// This might, however, be slower than adding the data normally, but can save on code size and it may come in handy
+// in rare scenarios where the state of the items source is unknown.
+var result = DiffUtil.CalculateDiff(data, people, diffCallback);
+
+// Apply the updates.
+result.DispatchUpdatesTo(data);
+
+Console.WriteLine();
+
+foreach (var item in data)
+    Console.WriteLine(item.ToString());
+
+Console.WriteLine();
+
+// Some time has passed - the remote data has changed.
+await Task.Delay(TimeSpan.FromSeconds(2));
+
+// Get the new data.
+people = await API.GetPeopleAsync();
+
+result = DiffUtil.CalculateDiff(data, people, diffCallback);
+
+result.DispatchUpdatesTo(data);
+
+Console.WriteLine();
+
+foreach (var item in data)
+    Console.WriteLine(item.ToString());
+
+Console.ReadLine();
