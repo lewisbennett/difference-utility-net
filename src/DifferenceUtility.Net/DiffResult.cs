1a52911a84dc04bfc1a7f04e370e0132bf2fc221
Lewis <lewisbennett669@gmail.com>
2021-12-11 23:25:55 +0000

    First working implementation
    Tested with 5 different collections.

--- src/DifferenceUtility.Net/DiffResult.cs
+++ src/DifferenceUtility.Net/DiffResult.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Xml;
 using DifferenceUtility.Net.Base;
 using DifferenceUtility.Net.CollectionUpdateCallbacks;
 using DifferenceUtility.Net.Helper;
@@ -18,7 +20,9 @@ namespace DifferenceUtility.Net
         private readonly IList<int> _path;
         private readonly IDiffCallback<TOld, TNew> _diffCallback;
         private readonly TNew[] _newArray;
+        private SortedDictionary<int, (int From, int Offset)> _offsets;
         private readonly TOld[] _oldArray;
+        private Dictionary<(int X, int Y), int> _postponedOperations;
         #endregion
         
         #region Public Methods
@@ -52,191 +56,119 @@ namespace DifferenceUtility.Net
             
             if (updateCallback is not BatchingCollectionUpdateCallback batchingCallback)
                 batchingCallback = new BatchingCollectionUpdateCallback(updateCallback);
-
-            var postponedUpdates = new HashSet<(int X, int Y)>();
-
+            
+            _offsets = new SortedDictionary<int, (int From, int Offset)>();
+            _postponedOperations = new Dictionary<(int X, int Y), int>();
+            
             var currentX = -1;
             var currentY = -1;
-
-            var offsets = new Dictionary<int, int>();
-
-            foreach (var payload in _path)
+            
+            for (var operationId = 0; operationId < _path.Count; operationId++)
             {
+                var operation = _path[operationId];
+
                 // Diagonal movement.
-                if ((payload & DiffOperation.NoOperation) != 0)
+                if ((operation & DiffOperation.NoOperation) != 0)
                 {
                     // Diagonals don't contain any coordinates, so just increment normally.
                     currentX++;
                     currentY++;
-
-                    if ((payload & DiffOperation.Update) != 0)
-                    {
-                        var finalOffset = 0;
-                        
-                        // Apply any offsets.
-                        foreach (var (xPosition, offset) in offsets)
-                        {
-                            if (currentX + finalOffset >= xPosition)
-                                finalOffset += offset;
-                        }
-                        
-                        batchingCallback.OnChanged(currentX + finalOffset, currentY, 1);
-                    }
+                    
+                    if ((operation & DiffOperation.Update) != 0)
+                        batchingCallback.OnChanged(OffsetX(currentX), currentY, 1);
 
                     continue;
                 }
-
-                (int X, int Y) postponedUpdate;
-
+                
+                int postponedOperationId;
+                int postponedX;
+                int postponedY;
+                
                 // Vertical movement.
-                if ((payload & DiffOperation.Insert) != 0)
+                if ((operation & DiffOperation.Insert) != 0)
                 {
                     currentY++;
 
                     // Regular insertion if no move flag.
-                    if ((payload & DiffOperation.Move) == 0)
+                    if ((operation & DiffOperation.Move) == 0)
                     {
-                        var mutableCurrentY = currentY;
+                        var offsetY = OffsetY(currentY);
+                        
+                        batchingCallback.OnInserted(offsetY, currentY);
 
-                        // Subtract the number of postponed updates where the Y value is less than the current Y.
-                        // This is to correct for any moves that haven't happened yet that would normally affect
-                        // the current Y coordinate, had they been processed normally.
-                        foreach (var update in postponedUpdates)
-                        {
-                            if (update.Y < currentY)
-                                mutableCurrentY--;
-                        }
-                        
-                        batchingCallback.OnInserted(mutableCurrentY, currentY);
-                        // batchingCallback.OnInserted(mutableCurrentY, 1);
+                        CreateXOffset(offsetY, 1, operationId);
 
-                        if (offsets.TryGetValue(mutableCurrentY, out var existingOffset))
-                            offsets[mutableCurrentY] = existingOffset + 1;
-                        
-                        else
-                            offsets[mutableCurrentY] = 1;
-                        
                         continue;
                     }
 
                     // X coordinate encoded in payload.
-                    var x = payload >> DiffOperation.Offset;
+                    var x = operation >> DiffOperation.Offset;
 
-                    // Try to find an existing postponed update with the provided coordinates.
-                    if (!postponedUpdates.TryGetValue((x, currentY), out postponedUpdate))
+                    // Try to find an existing postponed operation with the provided coordinates.
+                    if (!_postponedOperations.TryGetValue((x, currentY), out postponedOperationId))
                     {
-                        postponedUpdates.Add((x, currentY));
+                        _postponedOperations[(x, currentY)] = operationId;
                         continue;
                     }
+                    
+                    postponedX = x;
+                    postponedY = currentY;
                 }
                 // Horizontal movement.
-                else if ((payload & DiffOperation.Remove) != 0)
+                else if ((operation & DiffOperation.Remove) != 0)
                 {
                     currentX++;
 
                     // Regular removal if no move flag.
-                    if ((payload & DiffOperation.Move) == 0)
+                    if ((operation & DiffOperation.Move) == 0)
                     {
-                        var removeAt = payload >> DiffOperation.Offset;
+                        var offsetX = OffsetX(currentX);
 
-                        // Apply any offsets.
-                        foreach (var (xPosition, offset) in offsets)
-                        {
-                            if (removeAt >= xPosition)
-                                removeAt += offset;
-                        }
-                        
-                        batchingCallback.OnRemoved(removeAt, 1);
-                        
-                        if (offsets.TryGetValue(removeAt, out var existingOffset))
-                            offsets[removeAt] = existingOffset - 1;
-                        
-                        else
-                            offsets[removeAt] = -1;
+                        batchingCallback.OnRemoved(offsetX, 1);
+
+                        CreateXOffset(offsetX, -1, operationId);
 
                         continue;
                     }
 
                     // Y coordinate encoded in payload.
-                    var y = payload >> DiffOperation.Offset;
-
-                    // Try to find an existing postponed update with the provided coordinates.
-                    if (!postponedUpdates.TryGetValue((currentX, y), out postponedUpdate))
+                    var y = operation >> DiffOperation.Offset;
+                    
+                    // Try to find an existing postponed operation with the provided coordinates.
+                    if (!_postponedOperations.TryGetValue((currentX, y), out postponedOperationId))
                     {
-                        postponedUpdates.Add((currentX, y));
+                        _postponedOperations[(currentX, y)] = operationId;
                         continue;
                     }
+
+                    postponedX = currentX;
+                    postponedY = y;
                 }
                 else
                     continue;
 
-                postponedUpdates.Remove(postponedUpdate);
+                _postponedOperations.Remove((postponedX, postponedY));
 
-                // Apply any offsets.
-                foreach (var (xPosition, offset) in offsets)
+                // Apply offsets.
+                var offsetPostponedX = OffsetX(postponedX);
+                var offsetPostponedY = OffsetY(postponedY);
+                
+                if ((operation & DiffOperation.Update) != 0)
+                    batchingCallback.OnChanged(offsetPostponedX, postponedY, 1);
+
+                batchingCallback.OnMoved(offsetPostponedX, offsetPostponedY);
+
+                // Create the offsets as a result of the move operation.
+                if (offsetPostponedX > offsetPostponedY)
                 {
-                    if (postponedUpdate.X >= xPosition)
-                        postponedUpdate.X += offset;
+                    CreateXOffset(offsetPostponedY + 1, 1, postponedOperationId);
+                    CreateXOffset(offsetPostponedX + 1, -1, operationId);
                 }
-
-                var yOffset = 0;
-                
-                foreach (var (updateX, updateY) in postponedUpdates)
+                else if (offsetPostponedX < offsetPostponedY)
                 {
-                    // Future moves (postponed updates) may affect our current target index (Y coordinate of the postponed update we're handling).
-                    // Such operations will have a start (X coordinate) and finish (Y coordinate) position on either side of our current target index.
-                    // Operations that exist purely on one side of our current target index will not affect the current target index when they are handled.
-                    // We must apply an offset to counter the effect of any future operations that will affect our current target index in order for items
-                    // to be moved correctly now, and result in the correct position later. Refer to the below example:
-                    
-                    // Source:      B A D F H L O C Z
-                    // Destination: A Z H B D F C
-
-                    // During the path for the above data, we reach a point where the collection looks like this: A B D F H L O C Z
-                    // As you can see in the destination, 'H' comes before the sequence 'B D F', which has already been established due to prior moves.
-                    // In the calculated path, 'H' is instructed to move to position 2, which is its final position in the destination collection, and
-                    // also where it would already be if move detection was disabled. Since we're using move detection however, this position requires
-                    // offsetting in order to be correct by the end of the sequence.
-                    
-                    // We can see that 'Z', which is currently located at the end of the collection, comes before 'H' in the final destination. This is
-                    // a prime example of the offset described above and calculated below. The operation to move 'Z' is currently postponed and therefore,
-                    // 'Z' is not located at position 1 yet, which means that 'H' would be in the wrong position if we were to move it to the exact value
-                    // encoded in its payload. The postponed update for 'Z' has coordinates X8, Y1. The target index for 'H' (2) fits between these values,
-                    // so now we know it will be affected when the postponed update for 'Z' is handled. Since the target index for 'Z' (1) is less than 'H',
-                    // we know the position of 'H' will increase, so we negatively offset the target index for 'H' in order for everything to line up later.
-                    
-                    // This is confusing, I know.
-
-                    var mutableUpdateX = updateX;
-                    
-                    foreach (var (xPosition, offset) in offsets)
-                    {
-                        if (mutableUpdateX >= xPosition)
-                            mutableUpdateX += offset;
-                    }
-
-                    if (mutableUpdateX <= postponedUpdate.Y)
-                    {
-                        yOffset++;
-                    }
-                    else if (updateY < postponedUpdate.Y)
-                    {
-                        yOffset--;
-                    }
-
-                    // if (updateX <= postponedUpdate.X && updateY >= postponedUpdate.Y)
-                    //     yOffset++;
-                    //
-                    // else if (updateX >= postponedUpdate.X && updateY <= postponedUpdate.Y)
-                    //     yOffset--;
+                    CreateXOffset(offsetPostponedX, -1, postponedOperationId);
+                    CreateXOffset(offsetPostponedY, 1, operationId);
                 }
-
-                postponedUpdate.Y += yOffset;
-                
-                if ((payload & DiffOperation.Update) != 0)
-                    batchingCallback.OnChanged(postponedUpdate.X, postponedUpdate.Y, 1);
-                
-                batchingCallback.OnMoved(postponedUpdate.X, postponedUpdate.Y);
             }
 
             batchingCallback.DispatchLastEvent();
@@ -254,9 +186,60 @@ namespace DifferenceUtility.Net
         #endregion
         
         #region Private Methods
+        private void CreateXOffset(int from, int offset, int operationId)
+        {
+            for (var i = 0; i < _offsets.Count; i++)
+            {
+                var (queryOperationId, (queryFrom, queryOffset)) = _offsets.ElementAt(i);
+
+                // Offset the offset's 'from' position if it's affected by the new offset.
+                if (queryFrom > from)
+                    _offsets[queryOperationId] = (queryFrom + offset, queryOffset);
+            }
+            
+            // Finally, create the offset.
+            _offsets[operationId] = (from, offset);
+        }
+        
         private bool IsEmpty()
         {
             return _path is null || _path.Count == 0 || _diffCallback is null || _oldArray is null || _newArray is null || _oldArray.Length == 0 && _newArray.Length == 0;
+        }
+        
+        private int OffsetX(int x)
+        {
+            foreach (var (_, (from, offset)) in _offsets)
+            {
+                if (x >= from)
+                    x += offset;
+            }
+
+            return x;
+        }
+
+        private int OffsetY(int y)
+        {
+            var yOffset = 0;
+
+            foreach (var ((operationX, operationY), _) in _postponedOperations)
+            {
+                var offsetOperationX = OffsetX(operationX);
+
+                // The postponed operation's X value would increase if we were to insert at the current adjusted Y,
+                // and the postponed operation's X value is greater than or equal to the adjusted Y.
+                if (offsetOperationX >= y + yOffset)
+                    offsetOperationX++;
+                
+                // Decrement the offset for operations who's Y value is less than Y and are currently located after Y, relative to the adjusted position.
+                if (operationY < y && offsetOperationX > y + yOffset)
+                    yOffset--;
+                
+                // Increment the offset for operations who's Y value is greater than Y and are currently located before Y, relative to the adjusted position.
+                else if (operationY > y && offsetOperationX < y + yOffset)
+                    yOffset++;
+            }
+            
+            return y + yOffset;
         }
         #endregion
         
